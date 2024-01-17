cccad07ebdeb8ba5c1b3fa02f7efc90da315ce39
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2024-01-18 00:52:38 +0530

    added modal

---
+++ src/components/FormAdd/index.tsx
@@ -0,0 +1,171 @@
+import {
+  AlertErrorIcon,
+  SelectLabel,
+  TextField,
+  TrashCanIcon,
+} from "@saurabh-chauhan/sc-components-library";
+import React from "react";
+import {
+  Controller,
+  useFieldArray,
+  useFormContext,
+  useWatch,
+} from "react-hook-form";
+import { FIELD_REQUIRED, FIELD_TYPE_OPTIONS } from "../../helpers/constants";
+import css from "./index.module.scss";
+import { Button } from "@mui/joy";
+
+const AddFormComponents: React.FC = () => {
+  const { control } = useFormContext();
+
+  const { fields, remove, append } = useFieldArray({
+    name: "options",
+    control,
+  });
+
+  const watchFields = useWatch({
+    name: ["type"],
+    control,
+  });
+
+  const handleAddOptions = () => {
+    append({
+      label: "",
+      value: "",
+    });
+  };
+
+  const IsDropdown = watchFields[0].value === "Dropdown";
+
+  return (
+    <div className={css.container}>
+      <Controller
+        name="label"
+        control={control}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            required
+            label="Field Name"
+            placeholder="Enter field name.."
+            error={fieldState.invalid}
+            helperText={fieldState.error?.message}
+            endIcon={fieldState.invalid && <AlertErrorIcon />}
+          />
+        )}
+      />
+      <Controller
+        name="type"
+        control={control}
+        render={({ field, fieldState }) => (
+          <SelectLabel
+            {...field}
+            required
+            options={FIELD_TYPE_OPTIONS}
+            label="Field Type"
+            placeholder="Select field type.."
+            error={fieldState.invalid}
+            helperText={fieldState.error?.message}
+          />
+        )}
+      />
+      <section>
+        {IsDropdown && (
+          <Button
+            type="button"
+            variant="outlined"
+            color="success"
+            onClick={handleAddOptions}
+          >
+            Add Options
+          </Button>
+        )}
+        {IsDropdown &&
+          fields.map((field, index) => {
+            return (
+              <div key={field.id}>
+                <Controller
+                  name={`options.${index}.label`}
+                  control={control}
+                  render={({ field, fieldState }) => (
+                    <TextField
+                      {...field}
+                      required
+                      label="Label"
+                      placeholder="Enter label.."
+                      error={fieldState.invalid}
+                      helperText={fieldState.error?.message}
+                      endIcon={fieldState.invalid && <AlertErrorIcon />}
+                    />
+                  )}
+                />
+                <Controller
+                  name={`options.${index}.value`}
+                  control={control}
+                  render={({ field, fieldState }) => (
+                    <TextField
+                      {...field}
+                      required
+                      label="Value"
+                      placeholder="Enter value.."
+                      error={fieldState.invalid}
+                      helperText={fieldState.error?.message}
+                      endIcon={fieldState.invalid && <AlertErrorIcon />}
+                    />
+                  )}
+                />
+                <TrashCanIcon onClick={() => remove(index)} />
+              </div>
+            );
+          })}
+      </section>
+      <Controller
+        name="isRequired"
+        control={control}
+        render={({ field, fieldState }) => (
+          <SelectLabel
+            {...field}
+            required
+            options={FIELD_REQUIRED}
+            label="Field Required"
+            placeholder="Is field required?"
+            error={fieldState.invalid}
+            helperText={fieldState.error?.message}
+          />
+        )}
+      />
+      <Controller
+        name="maxLength"
+        control={control}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            required
+            label="Max Length"
+            placeholder="Enter max number of character in field.."
+            error={fieldState.invalid}
+            helperText={fieldState.error?.message}
+            endIcon={fieldState.invalid && <AlertErrorIcon />}
+          />
+        )}
+      />
+      <Controller
+        name="minLength"
+        control={control}
+        render={({ field, fieldState }) => (
+          <TextField
+            {...field}
+            required
+            label="Min Length"
+            placeholder="Enter min number of character in field.."
+            error={fieldState.invalid}
+            helperText={fieldState.error?.message}
+            endIcon={fieldState.invalid && <AlertErrorIcon />}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default AddFormComponents;
---
+++ src/views/dashboard/index.tsx
@@ -0,0 +1,74 @@
+import { Button, Modal, ModalDialog } from "@mui/joy";
+import React, { useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import AddFormComponents from "../../components/FormAdd";
+import css from "./index.module.scss";
+
+interface IFormGenerator {
+  label: string;
+  type: string;
+  isRequired: boolean;
+  maxLength: string;
+  minLength: string;
+  options?: { label: string; value: string }[];
+}
+
+const Dashboard: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const formGenerator = useForm<IFormGenerator>({
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+    defaultValues: {
+      label: "",
+      type: "",
+      isRequired: false,
+      maxLength: "",
+      minLength: "",
+      options: [{ label: "", value: "" }],
+    },
+  });
+
+  return (
+    <div className={css.container}>
+      <Modal open={open} disableAutoFocus onClose={() => setOpen(!open)}>
+        <ModalDialog
+          color="primary"
+          layout="center"
+          size="lg"
+          variant="outlined"
+        >
+          <div className={css.formWrapper}>
+            <form noValidate>
+              <FormProvider {...formGenerator}>
+                <AddFormComponents />
+              </FormProvider>
+              <section className={css.btnContainer}>
+                <Button
+                  variant="soft"
+                  type="submit"
+                  color="success"
+                  className={css.button}
+                >
+                  Submit
+                </Button>
+                <Button
+                  variant="outlined"
+                  type="button"
+                  color="warning"
+                  className={css.button}
+                  onClick={() => setOpen(!open)}
+                >
+                  Cancel
+                </Button>
+              </section>
+            </form>
+          </div>
+        </ModalDialog>
+      </Modal>
+
+      <Button onClick={() => setOpen(!open)}>Add Field</Button>
+    </div>
+  );
+};
+
+export default Dashboard;
