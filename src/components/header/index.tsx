e32f2c92af290eb51068ccfce426e8f66c441435
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2024-01-18 20:34:56 +0530

    build fixed

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,6 @@
-import { CssBaseline } from "@mui/joy";
+import { Typography } from "@saurabh-chauhan/sc-components-library";
 import React from "react";
 import css from "./index.module.scss";
-import { Typography } from "@saurabh-chauhan/sc-components-library";
 
 const Header: React.FC = () => {
   return (
